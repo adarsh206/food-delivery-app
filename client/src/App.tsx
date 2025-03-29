ea44ba695952728f3c3f79f24e1ec75aa664605a
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2025-03-29 11:17:35 +0530

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button className="bg-gradient-to-r from-purple-600 to-pink-600 
+      hover:bg-hoverColor">
+        Let's Build Food Delivery App
+      </Button>
+    </>
+  );
+}
+
+export default App;
