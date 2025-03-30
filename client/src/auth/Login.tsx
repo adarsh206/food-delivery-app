238201d681c016d99cb190038f51639932a61551
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2025-03-30 12:06:41 +0530

    signup

--- client/src/auth/Login.tsx
+++ client/src/auth/Login.tsx
@@ -3,13 +3,32 @@ import { Input } from "../components/ui/input";
 import { Button } from "../components/ui/button";
 import { Separator } from "../components/ui/separator";
 import { Link } from "react-router-dom";
+import { ChangeEvent, FormEvent, useState } from "react";
 
+type LoginInputState = {
+  email: string;
+  password: string;
+}
 const Login = () => {
+  const [input, setInput] = useState<LoginInputState>({
+    email: "",
+    password: "",
+  })
+
+  const changeEventHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const {name, value } = e.target;
+    setInput({...input, [name]: value });
+  }
+
+  const loginSubmitHandler = (e:FormEvent) => {
+    e.preventDefault();
+    console.log(input)
+  }
     const loading = false;
 
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <form className="md:p-8 w-full max-w-md md:border border-gray-200 rounded-lg mx-4">
+      <form onSubmit={loginSubmitHandler} className="md:p-8 w-full max-w-md md:border border-gray-200 rounded-lg mx-4">
         <div className="mb-4">
           <h1 className="font-bold inline-block text-2xl bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text text-transparent">
             AdarshEats
@@ -20,6 +39,9 @@ const Login = () => {
             <Input
               type="email"
               placeholder="Email"
+              name="email"
+              value={input.email}
+              onChange={changeEventHandler}
               className="pl-10 focus-visible:ring-1"
             />
             <Mail className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
@@ -30,6 +52,9 @@ const Login = () => {
             <Input
               type="password"
               placeholder="Password"
+              name="password"
+              value={input.password}
+              onChange={changeEventHandler}
               className="pl-10 focus-visible:ring-1"
             />
             <LockKeyhole className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
@@ -37,15 +62,15 @@ const Login = () => {
         </div>
         <div className="mb-10">
             {
-                loading ? <Button className="w-full bg-orange hover:bg-hoverColor"><Loader2 className="mr-2 h-4 w-4 animate-spin"/>Please wait</Button> : (
-                    <Button className="w-full bg-orange hover:bg-hoverColor">Login</Button>
+                loading ? <Button disabled className="w-full bg-orange hover:bg-hoverColor"><Loader2 className="mr-2 h-4 w-4 animate-spin"/>Please wait</Button> : (
+                    <Button type="submit" className="w-full bg-orange hover:bg-hoverColor">Login</Button>
                 )
             }    
         </div>
         <Separator />
         <p className="mt-2">
             Don't have an account?
-            <Link to="/signup" className="text-blue-600">Signup</Link>
+            <Link to="/signup" className="text-blue-600 ml-1">Signup</Link>
         </p>
       </form>
     </div>
