6c3c1ac84015170c729ed2c276d92746eaa97073
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2025-04-04 23:43:24 +0530

    EditMenu page added

--- client/src/admin/AddMenu.tsx
+++ client/src/admin/AddMenu.tsx
@@ -14,8 +14,49 @@ import { Label } from "../components/ui/label";
 import { Input } from "../components/ui/input";
 import EditMenu from "./EditMenu";
 
+const menus = [
+  {
+    name: "Biryani",
+    description: "Delicious biryani for you only.",
+    price: "200",
+    image:
+      "https://thumbs.dreamstime.com/b/assorted-indian-recipes-food-various-spices-rice-wooden-table-92742528.jpg",
+  },
+  {
+    name: "Momos",
+    description: "Delicious Momos for you only.",
+    price: "100",
+    image:
+      "https://thumbs.dreamstime.com/b/assorted-indian-recipes-food-various-spices-rice-wooden-table-92742528.jpg",
+  },
+  {
+    name: "Veggies",
+    description: "Delicious Veggies for you only.",
+    price: "250",
+    image:
+      "https://thumbs.dreamstime.com/b/assorted-indian-recipes-food-various-spices-rice-wooden-table-92742528.jpg",
+  },
+];
 const AddMenu = () => {
+  const [input, setInput] = useState<any>({
+    name: "",
+    description: "",
+    price: 0,
+    image: undefined,
+  });
+
+  const changeEventHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value, type } = e.target;
+    setInput({ ...input, [name]: type === "number" ? Number(value) : value });
+  };
+
+  const submitHandler = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(input);
+  };
+
   const [open, setOpen] = useState<boolean>(false);
+  const [editOpen, setEditOpen] = useState<boolean>(false);
   const [selectedMenu, setSelectedMenu] = useState<any>();
   const loading = false;
 
@@ -39,12 +80,14 @@ const AddMenu = () => {
                 Create a menu that will make your restaurant stand out.
               </DialogDescription>
             </DialogHeader>
-            <form action="">
+            <form onSubmit={submitHandler}>
               <div className="my-4">
                 <Label className="mb-2">Name</Label>
                 <Input
                   type="text"
                   name="name"
+                  value={input.name}
+                  onChange={changeEventHandler}
                   placeholder="Enter menu name"
                   className="focus-visible:ring-0 focus-visible:border-amber-500 focus-visible:shadow-amber-500 focus-visible:shadow-lg"
                 />
@@ -54,6 +97,8 @@ const AddMenu = () => {
                 <Input
                   type="text"
                   name="description"
+                  value={input.description}
+                  onChange={changeEventHandler}
                   placeholder="Enter menu description"
                   className="focus-visible:ring-0 focus-visible:border-amber-500 focus-visible:shadow-amber-500 focus-visible:shadow-lg"
                 />
@@ -63,6 +108,8 @@ const AddMenu = () => {
                 <Input
                   type="text"
                   name="price"
+                  value={input.price}
+                  onChange={changeEventHandler}
                   placeholder="Enter menu price"
                   className="focus-visible:ring-0 focus-visible:border-amber-500 focus-visible:shadow-amber-500 focus-visible:shadow-lg"
                 />
@@ -72,6 +119,12 @@ const AddMenu = () => {
                 <Input
                   type="file"
                   name="image"
+                  onChange={(e) =>
+                    setInput({
+                      ...input,
+                      image: e.target.files?.[0] || undefined,
+                    })
+                  }
                   placeholder="Enter menu image"
                 />
               </div>
@@ -91,29 +144,42 @@ const AddMenu = () => {
           </DialogContent>
         </Dialog>
       </div>
-      <div className="mt-6 space-y-4">
-        <div className="flex flex-col md:flex-row md:items-center md:space-x-4 md:p-4 p-2 shadow-md rounded-lg border">
-          <img
-            src="https://thumbs.dreamstime.com/b/assorted-indian-recipes-food-various-spices-rice-wooden-table-92742528.jpg"
-            alt=""
-            className="md:h-24 md:w-24 h-16 w-full object-cover rounded-lg"
-          />
-          <div className="flex-1">
-            <h1 className="text-lg font-semibold text-gray-800">Biryani</h1>
-            <p className="text-sm tex-gray-600 mt-1">
-              Delicious biryani for you only.
-            </p>
-            <h2 className="text-md font-semibold mt-2">
-              Price: <span className="text-orange-600">80</span>
-            </h2>
+      {menus.map((menu: any, idx: number) => (
+        <div key={idx} className="mt-6 space-y-4">
+          <div className="flex flex-col md:flex-row md:items-center md:space-x-4 md:p-4 p-2 shadow-md rounded-lg border">
+            <img
+              src={menu.image}
+              alt=""
+              className="md:h-24 md:w-24 h-16 w-full object-cover rounded-lg"
+            />
+            <div className="flex-1">
+              <h1 className="text-lg font-semibold text-gray-800">
+                {menu.name}
+              </h1>
+              <p className="text-sm tex-gray-600 mt-1">{menu.description}</p>
+              <h2 className="text-md font-semibold mt-2">
+                Price: <span className="text-orange-600">{menu.price}</span>
+              </h2>
+            </div>
+            <Button
+              onClick={() => {
+                setSelectedMenu(menu);
+                setEditOpen(true);
+              }}
+              size={"sm"}
+              className="bg-orange hover:bg-hoverColor mt-2"
+            >
+              Edit
+            </Button>
           </div>
-          <Button size={"sm"} className="bg-orange hover:bg-hoverColor mt-2">
-            Edit
-          </Button>
         </div>
-      </div>
-      <EditMenu selectedMenu = {selectedMenu}
-        />
+      ))}
+
+      <EditMenu
+        selectedMenu={selectedMenu}
+        editOpen={editOpen}
+        setEditOpen={setEditOpen}
+      />
     </div>
   );
 };
--- client/src/admin/EditMenu.tsx
+++ client/src/admin/EditMenu.tsx
@@ -1,10 +1,126 @@
+import { Dispatch, FormEvent, SetStateAction, useEffect, useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "../components/ui/dialog";
+import { Label } from "../components/ui/label";
+import { Input } from "../components/ui/input";
+import { Loader2 } from "lucide-react";
+import { Button } from "../components/ui/button";
 
-const EditMenu = () => {
+const EditMenu = ({
+  selectedMenu,
+  editOpen,
+  setEditOpen,
+}: {
+  selectedMenu: any;
+  editOpen: boolean;
+  setEditOpen: Dispatch<SetStateAction<boolean>>;
+}) => {
+    
+    const [input, setInput] = useState<any>({
+        name: "",
+        description: "",
+        price: 0,
+        image: undefined,
+      });
+      
+      const loading = false;
+      const changeEventHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value, type } = e.target;
+        setInput({ ...input, [name]: type === "number" ? Number(value) : value });
+      };
+    
+    const submitHandler = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+    }
+
+    useEffect(() => { 
+        setInput({
+          name: selectedMenu?.name || "",
+          description: selectedMenu?.description || "",
+          price: selectedMenu?.price || 0,
+          image: undefined,
+        });
+      }, [selectedMenu]);
+
   return (
-    <div>
-        dfsdf
-    </div>
-  )
-}
+    <Dialog open={editOpen} onOpenChange={setEditOpen}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Edit Menu</DialogTitle>
+          <DialogDescription>
+            Update your menu to keep your offerings fresh and exciting!
+          </DialogDescription>
+        </DialogHeader>
+        <form onSubmit={submitHandler}>
+              <div className="my-4">
+                <Label className="mb-2">Name</Label>
+                <Input
+                  type="text"
+                  name="name"
+                  value={input.name}
+                  onChange={changeEventHandler}
+                  placeholder="Enter menu name"
+                  className="focus-visible:ring-0 focus-visible:border-amber-500 focus-visible:shadow-amber-500 focus-visible:shadow-lg"
+                />
+              </div>
+              <div className="my-4">
+                <Label className="mb-2">Description</Label>
+                <Input
+                  type="text"
+                  name="description"
+                  value={input.description}
+                  onChange={changeEventHandler}
+                  placeholder="Enter menu description"
+                  className="focus-visible:ring-0 focus-visible:border-amber-500 focus-visible:shadow-amber-500 focus-visible:shadow-lg"
+                />
+              </div>
+              <div className="my-4">
+                <Label className="mb-2">Price in (Rupees)</Label>
+                <Input
+                  type="text"
+                  name="price"
+                  value={input.price}
+                  onChange={changeEventHandler}
+                  placeholder="Enter menu price"
+                  className="focus-visible:ring-0 focus-visible:border-amber-500 focus-visible:shadow-amber-500 focus-visible:shadow-lg"
+                />
+              </div>
+              <div className="my-4">
+                <Label className="mb-2">Upload Menu Image</Label>
+                <Input
+                  type="file"
+                  name="image"
+                  onChange={(e) =>
+                    setInput({
+                      ...input,
+                      image: e.target.files?.[0] || undefined,
+                    })
+                  }
+                  placeholder="Enter menu image"
+                />
+              </div>
+              <DialogFooter className="mt-5">
+                {loading ? (
+                  <Button disabled className="bg-orange hover:bg-hoverColor">
+                    <Loader2 className="mr-2 w-4 h-4 animate-spin" />
+                    Please wait
+                  </Button>
+                ) : (
+                  <Button className="bg-orange hover:bg-hoverColor">
+                    Submit
+                  </Button>
+                )}
+              </DialogFooter>
+            </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
 
-export default EditMenu
+export default EditMenu;
